aad5fe28181df011bee78d61c260303804067ec2
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2023-01-16 21:30:39 +0100

    INT: allows to reload counter data.

--- app/src/main/java/com/app/bbcounters/MainActivity.kt
+++ app/src/main/java/com/app/bbcounters/MainActivity.kt
@@ -2,16 +2,19 @@ package com.app.bbcounters
 
 import android.content.Context
 import android.os.Bundle
+import android.support.v4.view.GestureDetectorCompat
+import android.support.v4.view.MotionEventCompat
 import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.OnScrollListener
+import android.support.v7.widget.helper.ItemTouchHelper
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import android.widget.Toast
 import java.util.concurrent.Executors
 
 
@@ -41,29 +44,81 @@ class DeviceAdapter(private val devices : Array<DisplayedCountersData>,
     override fun getItemCount(): Int = devices.size
 }
 
-class MainActivity : AppCompatActivity() {
-    var recyclerView : RecyclerView? = null
+class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
+    private var recyclerView : RecyclerView? = null
+    private lateinit var detector : GestureDetectorCompat
+    private var onTop = true
+    private var toast: Toast? = null
     override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
-            val actionBar = getSupportActionBar()
-            if (actionBar != null) {
-                actionBar.setDisplayShowHomeEnabled(true)
-                actionBar.setIcon(R.mipmap.ic_launcher_round)
-            }
+            showAppIcon()
+            detector = GestureDetectorCompat(this,  this)
             recyclerView = findViewById<RecyclerView>(R.id.devicesList)
             val linearLayoutManager = LinearLayoutManager(this)
             recyclerView?.layoutManager = linearLayoutManager
-        val dividerItemDecoration = DividerItemDecoration(
-            recyclerView?.getContext(),
-            linearLayoutManager.getOrientation()
-        )
+            val dividerItemDecoration = DividerItemDecoration(
+                recyclerView?.getContext(),
+                linearLayoutManager.getOrientation()
+            )
         recyclerView?.addItemDecoration(dividerItemDecoration)
-            Executors.newSingleThreadExecutor().execute {
-                val devicesDataArray = DisplayedCountersData.get()
-                runOnUiThread {
-                    recyclerView?.adapter = DeviceAdapter(devicesDataArray, this)
-                }
+        recyclerView?.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(p0: View?, p1: MotionEvent?): Boolean = detector.onTouchEvent(p1)
+        })
+        recyclerView?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                onTop = (!recyclerView.canScrollVertically(-1) && dy < 0)
             }
+        })
+        loadList()
     }
+
+    private fun showAppIcon() {
+        val actionBar = getSupportActionBar()
+        if (actionBar != null) {
+            actionBar.setDisplayShowHomeEnabled(true)
+            actionBar.setIcon(R.mipmap.ic_launcher_round)
+        }
+    }
+
+    private fun loadList() {
+        Executors.newSingleThreadExecutor().execute {
+            val devicesDataArray = DisplayedCountersData.get()
+            runOnUiThread {
+                recyclerView?.adapter = DeviceAdapter(devicesDataArray, this)
+                toast?.cancel()
+                toast = null
+            }
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (detector.onTouchEvent(event)) {
+            true
+        } else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    override fun onDown(p0: MotionEvent?): Boolean = false
+
+    override fun onShowPress(p0: MotionEvent?) { }
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean = false
+
+    override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return if (onTop && toast == null) {
+            toast = Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT)
+            toast?.show()
+            loadList()
+            true
+        } else
+            false
+    }
+
+    override fun onLongPress(p0: MotionEvent?) { }
+
+    override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean = false
+
 }
