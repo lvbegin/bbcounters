92aa1085744edc83a1c53ecc28c0eba3da58c437
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2023-02-26 17:24:21 +0100

    INT: do not reload year history of a device when changing orientation.

--- app/src/main/java/com/app/bbcounters/YearCounterActivity.kt
+++ app/src/main/java/com/app/bbcounters/YearCounterActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.AdapterView
@@ -20,12 +22,36 @@ import java.util.concurrent.Executors
 import java.util.stream.Collectors
 import java.util.stream.IntStream
 
+class ParcelableEntry(x : Float, y : Float) : Entry(x, y), Parcelable {
+    constructor(parcel: Parcel) : this(parcel.readFloat(), parcel.readFloat()) { }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        super.writeToParcel(parcel, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ParcelableEntry> {
+        override fun createFromParcel(parcel: Parcel): ParcelableEntry {
+            return ParcelableEntry(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ParcelableEntry?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
+
 class YearCounterActivity : android.support.v7.app.AppCompatActivity() {
     private lateinit var lineChart: LineChart
     private lateinit var id : String
     private lateinit var listYears : ArrayList<String>
     private lateinit var spinner : Spinner
-
+    private var entries : List<ParcelableEntry>? = null
+    private var currentYearIndex : Int? = null;
     companion object {
         fun startActivity(context: Context, id : String, listYears: ArrayList<String>) {
             val intent = Intent(context, YearCounterActivity::class.java)
@@ -39,14 +65,17 @@ class YearCounterActivity : android.support.v7.app.AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_year_counter)
         setIcon(this)
+        val savedEntries : ArrayList<ParcelableEntry>? = savedInstanceState?.getParcelableArrayList("entries")
+        entries = savedEntries?.toList()
+
         id = intent.extras?.getString("id") ?: return
         listYears = intent.getSerializableExtra("years") as ArrayList<String>
         listYears.sort()
+        val yearIndex = savedInstanceState?.getInt("current") ?: listYears.size - 1
         lineChart = findViewById<LineChart>(R.id.yearlyHistoryChart)
         lineChart.setNoDataText(resources.getString(R.string.loading_data))
         lineChart.setNoDataTextColor(R.color.primaryTextColor)
         val adapter = ArrayAdapter<String>(this, R.layout.year_item_layout)
-//        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
         adapter.setDropDownViewResource(R.layout.year_item_list_layout)
         for (year in listYears) {
             adapter.add(year)
@@ -54,20 +83,31 @@ class YearCounterActivity : android.support.v7.app.AppCompatActivity() {
 
         spinner = findViewById<Spinner>(R.id.list_counter_years)
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 val year : String = adapter.getItem(position) ?: return
-                this@YearCounterActivity.loadDataIntoGraph(year)
+                if (this@YearCounterActivity.currentYearIndex != position) {
+                    this@YearCounterActivity.currentYearIndex = position
+                    entries = null
+                    this@YearCounterActivity.loadDataIntoGraph(year)
+                }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) { }
         }
         spinner.adapter = adapter
-        spinner.setSelection(listYears.size - 1)
+        currentYearIndex = yearIndex
+        spinner.setSelection(yearIndex)
+        loadDataIntoGraph(listYears.get(yearIndex))
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val savedEntries = entries
+        val savedCurrentYearIndex = currentYearIndex
+        if (savedEntries != null)
+            outState.putParcelableArrayList("entries", ArrayList<ParcelableEntry>(savedEntries))
+        if (savedCurrentYearIndex != null)
+            outState.putInt("current", savedCurrentYearIndex)
     }
 
     fun loadDataIntoGraph(year : String) {
@@ -77,17 +117,23 @@ class YearCounterActivity : android.support.v7.app.AppCompatActivity() {
         spinner.isEnabled = false
         Executors.newSingleThreadExecutor().execute {
 
-            var data = DataServer().getCounterHistoryYear(id, year)
-            if (data.isEmpty()) {
-                askIfRetry(this) { this@YearCounterActivity.loadDataIntoGraph(year) }
-                return@execute
+            if (entries == null) {
+                var data = DataServer().getCounterHistoryYear(id, year)
+                if (data.isEmpty()) {
+                    askIfRetry(this) { this@YearCounterActivity.loadDataIntoGraph(year) }
+                    return@execute
+                }
+
+                var keys = data.keys.toTypedArray()
+
+                entries = IntStream.range(0, data.keys.size).mapToObj { i ->
+                    ParcelableEntry(
+                        i.toFloat(),
+                        data.get(keys.get(i))?.toFloat() ?: 0f
+                    )
+                }.collect(Collectors.toList())
             }
-
-            var keys = data.keys.toTypedArray()
-
-            val entries = IntStream.range(0, data.keys.size).mapToObj { i -> Entry(i.toFloat(),
-                    data.get(keys.get(i))?.toFloat() ?: 0f) }.collect(Collectors.toList())
-            val dayValues = ArrayList<Entry>(entries)
+            val dayValues = entries as List<Entry>
             var lineDataSet = LineDataSet(dayValues, getString(R.string.graph_year_counter_label))
             lineDataSet.setDrawValues(false)
             lineDataSet.color = Color.BLUE
