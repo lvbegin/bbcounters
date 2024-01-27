d58b01950dafead688a3853a399e1818715e6ae5
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2024-01-27 22:31:13 +0100

    INT: layout of list of graphs.

--- app/src/main/java/com/app/bbcounters/graphShortcut.kt
+++ app/src/main/java/com/app/bbcounters/graphShortcut.kt
@@ -2,18 +2,22 @@ package com.app.bbcounters
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Window as Window
 
 fun graphShortcutCallback(context: Activity, data : Array<DisplayedCountersData>, position : Int,
                           x : Int, y : Int, gravity : Int) : Boolean {
     val a = arrayOf("History", "Year")
     val builder = AlertDialog.Builder(context, R.style.dialog_with_rounded_corner)
+        .setTitle("Graphs")
         .setItems(a) { _, which ->
             when (which) {
                 0 -> {
                     BikeCounterActivity.startActivity(context, data[position].name)
                     context.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
                 }
-                1-> {
+                1 -> {
                     YearCounterActivity.startActivity(context, data[position].name)
                     context.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
                 }
@@ -26,6 +30,7 @@ fun graphShortcutCallback(context: Activity, data : Array<DisplayedCountersData>
     dialog.window?.attributes?.y = y
 
     dialog.show()
-    dialog.window?.setLayout(500, 400)
+    dialog.window?.setLayout(500, dialog.window?.attributes?.height ?: 600)
+
     return true
 }
